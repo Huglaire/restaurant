714307914f5a987fc27b167dd60d647b64bed60a
Huglaire <pollon.hugo@gmail.com>
2026-06-02 15:36:34 +0200

    Création CRUD Restaurant

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -3,46 +3,95 @@
 namespace App\Controller;
 
 use App\Entity\Restaurant;
+use App\Repository\RestaurantRepository;
 use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('api/restaurant', name: 'app_api_restaurant_')]
-final class RestaurantController extends AbstractController
+#[Route('/api/restaurant', name: 'app_api_restaurant_')]
+class RestaurantController extends AbstractController
 {
-    #[Route('/', name: 'new', methods: 'POST')]
+    public function __construct(private EntityManagerInterface $manager, private RestaurantRepository $repository)
+    {
+    }
+
+    #[Route('/', name: 'new', methods: ['POST'])]
     public function new(): Response
     {
-       $restaurant = new Restaurant();
-       $restaurant->setName( name: 'Quai Antique');
-       $restaurant->setDescription( description: 'Cette qualité et ce goût par le chef Arnaud Michant');
-       $restaurant->setCreatedAt(new DateTimeImmutable());
+        $restaurant = new Restaurant();
+        $restaurant->setName('Quai Antique');
+        $restaurant->setDescription('Cette qualité et ce goût par le chef Arnaud MICHANT.');
+        $restaurant->setCreatedAt(new DateTimeImmutable());
 
-       // A stocker en base
+        $restaurant->setMaxGuest(50);
 
-       return $this->json(
-        ['message' => "Restaurant resource created with {$restaurant->getId()} id"],
-        Response::HTTP_CREATED,
-       );
+        $restaurant->setAmOpeningTime([
+           'open' => '09:00',
+           'close' => '14:00'
+         ]);
+
+        $restaurant->setPmOpeningTime([
+            'open' => '19:00',
+            'close' => '23:00'
+         ]);
+
+        // Tell Doctrine you want to (eventually) save the restaurant (no queries yet)
+        $this->manager->persist($restaurant);
+        // Actually executes the queries (i.e. the INSERT query)
+        $this->manager->flush();
+
+        return $this->json(
+            ['message' => "Restaurant resource created with id: {$restaurant->getId()}"],
+            Response::HTTP_CREATED,
+        );
     }
 
-    #[Route('/show', name: 'show', methods: 'GET')]
-    public function show(): Response
+
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
+    public function show(int $id): Response
     {
-       return $this->json(['message' => 'Restaurant de ma BDD']);
+        $restaurant = $this->repository->findOneBy(['id' => $id]);
+
+        if (!$restaurant) {
+            throw $this->createNotFoundException("No Restaurant found for id: {$id}");
+        }
+
+        return $this->json(
+            ['message' => "A Restaurant was found : {$restaurant->getName()} for id:; {$restaurant->getId()}"]
+        );
     }
-    
-    #[Route('/', name: 'edit', methods: 'PUT')]
-    public function edit(): Response
+
+
+    #[Route('/{id}', name: 'edit', methods: ['PUT'])]
+    public function edit(int $id): Response
     {
-        
+        $restaurant = $this->repository->findOneBy(['id' => $id]);
+
+        if (!$restaurant) {
+            throw $this->createNotFoundException("No Restaurant found for {$id} id");
+        }
+
+        $restaurant->setName('Restaurant name updated');
+        $this->manager->flush();
+
+        return $this->redirectToRoute('app_api_restaurant_show', ['id' => $restaurant->getId()]);
     }
-    #[Route('/', name: 'delete', methods: 'DELETE')]
-    public function delete(): Response
+
+    #[Route('/{id}', name: 'delete', methods: ['DELETE'])]
+    public function delete(int $id): Response
     {
-        
+        $restaurant = $this->repository->findOneBy(['id' => $id]);
+        if (!$restaurant) {
+            throw $this->createNotFoundException("No Restaurant found for {$id} id");
+        }
+
+        $this->manager->remove($restaurant);
+        $this->manager->flush();
+
+        return $this->json(['message' => "Restaurant resource deleted"], Response::HTTP_OK);
     }
 
 }
+
