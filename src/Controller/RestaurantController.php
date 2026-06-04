3a632a4ae17c4dcc2fd8fe17d618abd150e82976
Huglaire <pollon.hugo@gmail.com>
2026-06-04 14:57:33 +0200

    Ajout pack serializer + Nelmio (rendre API publique) et serialization des données RestaurantController

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -4,11 +4,16 @@ namespace App\Controller;
 
 use App\Entity\Restaurant;
 use App\Repository\RestaurantRepository;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/api/restaurant', name: 'app_api_restaurant_')]
 class RestaurantController extends AbstractController
@@ -16,18 +21,31 @@ class RestaurantController extends AbstractController
     public function __construct(
         private EntityManagerInterface $manager,
         private RestaurantRepository $repository,
+        private SerializerInterface $serializer,
+        private UrlGeneratorInterface $urlGenerator,
     ) {
     }
 
     #[Route('/', name: 'new', methods: ['POST'])]
-    public function new(): JsonResponse
+    public function new(Request $request): JsonResponse
     {
-        $this->manager->persist();
+        $restaurant = $this->serializer->deserialize($request->getContent(), type: Restaurant::class, format: 'json');
+        $restaurant->setCreatedAt(new DateTimeImmutable());
+
+        $this->manager->persist($restaurant);
         $this->manager->flush();
 
+        $responseData = $this->serializer->serialize($restaurant, format:'json');
+        $location = $this->urlGenerator->generate(
+            name: 'app_api_restaurant_show',
+            parameters: ['id' => $restaurant->getId()],
+            referenceType: UrlGeneratorInterface::ABSOLUTE_URL,
+        );
+
         return new JsonResponse(
-            data: null,
+            $responseData,
             status: Response::HTTP_CREATED,
+            headers: ["Location" => $location],
             json: true
         );
     }
@@ -36,10 +54,11 @@ class RestaurantController extends AbstractController
     public function show(int $id): JsonResponse
     {
         $restaurant = $this->repository->findOneBy(['id' => $id]);
-
         if ($restaurant) {
+            $responseData = $this->serializer->serialize($restaurant, 'json');
+
             return new JsonResponse(
-                data: null,
+                data: $responseData,
                 status: Response::HTTP_OK,
                 json: true
             );
@@ -52,11 +71,19 @@ class RestaurantController extends AbstractController
     }
 
     #[Route('/{id}', name: 'edit', methods: ['PUT'])]
-    public function edit(int $id): JsonResponse
+    public function edit(int $id, Request $request): JsonResponse
     {
         $restaurant = $this->repository->findOneBy(['id' => $id]);
 
         if ($restaurant) {
+            $restaurant = $this->serializer->deserialize(
+                $request->getContent(),
+                type: Restaurant::class,
+                format: 'json',
+                context: [AbstractNormalizer::OBJECT_TO_POPULATE => $restaurant]
+            );
+            $restaurant->setUpdatedAt(new DateTimeImmutable());
+            
             $this->manager->flush();
 
             return new JsonResponse(
