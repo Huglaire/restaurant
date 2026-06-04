7804118c89ffbfe6d497e9ab8e06c412d2e8fc31
Huglaire <pollon.hugo@gmail.com>
2026-06-04 10:41:32 +0200

    Changement RestaurantController: suppression "Exceptions" pour passer en JsonResponse

--- src/Controller/RestaurantController.php
+++ src/Controller/RestaurantController.php
@@ -4,94 +4,91 @@ namespace App\Controller;
 
 use App\Entity\Restaurant;
 use App\Repository\RestaurantRepository;
-use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/api/restaurant', name: 'app_api_restaurant_')]
 class RestaurantController extends AbstractController
 {
-    public function __construct(private EntityManagerInterface $manager, private RestaurantRepository $repository)
-    {
+    public function __construct(
+        private EntityManagerInterface $manager,
+        private RestaurantRepository $repository,
+    ) {
     }
 
     #[Route('/', name: 'new', methods: ['POST'])]
-    public function new(): Response
+    public function new(): JsonResponse
     {
-        $restaurant = new Restaurant();
-        $restaurant->setName('Quai Antique');
-        $restaurant->setDescription('Cette qualité et ce goût par le chef Arnaud MICHANT.');
-        $restaurant->setCreatedAt(new DateTimeImmutable());
-
-        $restaurant->setMaxGuest(50);
-
-        $restaurant->setAmOpeningTime([
-           'open' => '09:00',
-           'close' => '14:00'
-         ]);
-
-        $restaurant->setPmOpeningTime([
-            'open' => '19:00',
-            'close' => '23:00'
-         ]);
-
-        // Tell Doctrine you want to (eventually) save the restaurant (no queries yet)
-        $this->manager->persist($restaurant);
-        // Actually executes the queries (i.e. the INSERT query)
+        $this->manager->persist();
         $this->manager->flush();
 
-        return $this->json(
-            ['message' => "Restaurant resource created with id: {$restaurant->getId()}"],
-            Response::HTTP_CREATED,
+        return new JsonResponse(
+            data: null,
+            status: Response::HTTP_CREATED,
+            json: true
         );
     }
-
 
     #[Route('/{id}', name: 'show', methods: ['GET'])]
-    public function show(int $id): Response
+    public function show(int $id): JsonResponse
     {
         $restaurant = $this->repository->findOneBy(['id' => $id]);
 
-        if (!$restaurant) {
-            throw $this->createNotFoundException("No Restaurant found for id: {$id}");
+        if ($restaurant) {
+            return new JsonResponse(
+                data: null,
+                status: Response::HTTP_OK,
+                json: true
+            );
         }
 
-        return $this->json(
-            ['message' => "A Restaurant was found : {$restaurant->getName()} for id:; {$restaurant->getId()}"]
+        return new JsonResponse(
+            data: null,
+            status: Response::HTTP_NOT_FOUND
         );
     }
 
-
     #[Route('/{id}', name: 'edit', methods: ['PUT'])]
-    public function edit(int $id): Response
+    public function edit(int $id): JsonResponse
     {
         $restaurant = $this->repository->findOneBy(['id' => $id]);
 
-        if (!$restaurant) {
-            throw $this->createNotFoundException("No Restaurant found for {$id} id");
+        if ($restaurant) {
+            $this->manager->flush();
+
+            return new JsonResponse(
+                data: null,
+                status: Response::HTTP_NO_CONTENT
+            );
         }
 
-        $restaurant->setName('Restaurant name updated');
-        $this->manager->flush();
-
-        return $this->redirectToRoute('app_api_restaurant_show', ['id' => $restaurant->getId()]);
+        return new JsonResponse(
+            data: null,
+            status: Response::HTTP_NOT_FOUND
+        );
     }
 
     #[Route('/{id}', name: 'delete', methods: ['DELETE'])]
-    public function delete(int $id): Response
+    public function delete(int $id): JsonResponse
     {
         $restaurant = $this->repository->findOneBy(['id' => $id]);
-        if (!$restaurant) {
-            throw $this->createNotFoundException("No Restaurant found for {$id} id");
+
+        if ($restaurant) {
+            $this->manager->remove($restaurant);
+            $this->manager->flush();
+
+            return new JsonResponse(
+                data: null,
+                status: Response::HTTP_NO_CONTENT
+            );
         }
 
-        $this->manager->remove($restaurant);
-        $this->manager->flush();
-
-        return $this->json(['message' => "Restaurant resource deleted"], Response::HTTP_OK);
+        return new JsonResponse(
+            data: null,
+            status: Response::HTTP_NOT_FOUND
+        );
     }
-
 }
-
