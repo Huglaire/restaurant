52e821ac6e13b23fa8caff20d395258b8c76b3c2
Huglaire <pollon.hugo@gmail.com>
2026-06-12 10:15:40 +0200

    Ajout horaires pour restaurantFixtures

--- src/DataFixtures/RestaurantFixtures.php
+++ src/DataFixtures/RestaurantFixtures.php
@@ -17,13 +17,29 @@ class RestaurantFixtures extends Fixture
     {
         $faker = Factory::create('fr_FR');
 
+        $servicesMidi = [
+            ['open' => '11:30', 'close' => '14:00'],
+            ['open' => '12:00', 'close' => '14:30'],
+            ['open' => '12:00', 'close' => '15:00'],
+        ];
+
+        $servicesSoir = [
+            ['open' => '18:30', 'close' => '22:00'],
+            ['open' => '19:00', 'close' => '22:30'],
+            ['open' => '19:30', 'close' => '23:00'],
+        ];
+
         for ($i = 1; $i <= self::RESTAURANT_NB_TUPLES; $i++) {
 
             $restaurant = (new Restaurant())
-                ->setName($faker->company())
-                ->setDescription($faker->paragraph(3))
-                ->setAmOpeningTime([])
-                ->setPmOpeningTime([])
+                ->setName('Restaurant ' . $faker->lastName())
+                ->setDescription($faker->paragraphs(3, true))
+                ->setAmOpeningTime(
+                    $faker->randomElement($servicesMidi)
+                )
+                ->setPmOpeningTime(
+                    $faker->randomElement($servicesSoir)
+                )
                 ->setMaxGuest(random_int(10, 50))
                 ->setCreatedAt(new DateTimeImmutable());
 
